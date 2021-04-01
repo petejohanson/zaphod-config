c456b2ad667624b8d77a0b5acf355c1b2732e662
Peter Johanson <peter@peterjohanson.com>
2021-04-01 13:12:51 -0400

    Initial config.

---
+++ boards/arm/zaphod/zaphod_status_screen.c
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2020 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <zmk/display/widgets/output_status.h>
+#include <zmk/display/widgets/battery_status.h>
+#include <zmk/display/widgets/layer_status.h>
+#include <zmk/display/widgets/wpm_status.h>
+#include <zmk/display/status_screen.h>
+
+#include <logging/log.h>
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_BATTERY_STATUS)
+static struct zmk_widget_battery_status battery_status_widget;
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_OUTPUT_STATUS)
+static struct zmk_widget_output_status output_status_widget;
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_LAYER_STATUS)
+static struct zmk_widget_layer_status layer_status_widget;
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
+static struct zmk_widget_wpm_status wpm_status_widget;
+#endif
+
+lv_style_t global_style;
+
+lv_obj_t *zmk_display_status_screen() {
+    lv_obj_t *screen;
+    lv_obj_t *dont_label;
+    lv_obj_t *panic_label;
+    lv_obj_t *label_cont;
+
+    lv_style_init(&global_style);
+    lv_style_set_text_font(&global_style, LV_STATE_DEFAULT, &lv_font_montserrat_26);
+    lv_style_set_text_letter_space(&global_style, LV_STATE_DEFAULT, 1);
+    lv_style_set_text_line_space(&global_style, LV_STATE_DEFAULT, 1);
+
+    screen = lv_obj_create(NULL, NULL);
+    lv_obj_add_style(screen, LV_LABEL_PART_MAIN, &global_style);
+
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_BATTERY_STATUS)
+    zmk_widget_battery_status_init(&battery_status_widget, screen);
+    lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), NULL, LV_ALIGN_IN_TOP_RIGHT,
+                 0, 0);
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_OUTPUT_STATUS)
+    zmk_widget_output_status_init(&output_status_widget, screen);
+    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), NULL, LV_ALIGN_IN_TOP_LEFT, 0,
+                 0);
+#endif
+
+    label_cont = lv_cont_create(screen, NULL);
+    lv_obj_set_auto_realign(label_cont, true);
+    lv_obj_align(label_cont, NULL, LV_ALIGN_CENTER, 0,
+                 0);
+    lv_cont_set_fit(label_cont, LV_FIT_TIGHT);
+    lv_cont_set_layout(label_cont, LV_LAYOUT_COLUMN_MID);
+
+    dont_label = lv_label_create(label_cont, NULL);
+    // lv_label_set_align(dont_label, LV_LABEL_ALIGN_CENTER);
+    lv_label_set_text(dont_label, "Don't");
+
+    panic_label = lv_label_create(label_cont, NULL);
+    lv_label_set_text(panic_label, "Panic");
+    // lv_label_set_align(panic_label, LV_LABEL_ALIGN_CENTER);
+
+    /*
+    lv_obj_align(dont_label, NULL, LV_ALIGN_IN_TOP_MID, 0,
+                 0);
+    lv_obj_align(panic_label, NULL, LV_ALIGN_OUT_BOTTOM_MID, 0,
+                 0);
+		 */
+
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_LAYER_STATUS)
+    zmk_widget_layer_status_init(&layer_status_widget, screen);
+    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), NULL, LV_ALIGN_IN_BOTTOM_LEFT,
+                 0, 0);
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
+    zmk_widget_wpm_status_init(&wpm_status_widget, screen);
+    lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), NULL, LV_ALIGN_IN_BOTTOM_RIGHT, -12,
+                 0);
+#endif
+    return screen;
+}

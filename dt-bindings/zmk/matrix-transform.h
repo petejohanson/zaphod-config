30487e728f5225a9bd1b7a6c362e7a167a51737c
Jan Titze <jan.titze@gmail.com>
2022-10-04 12:17:35 +0200

    keymap 0.1

---
+++ dt-bindings/zmk/matrix-transform.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2020 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#warning "matrix-transform.h has been deprecated and superseded by matrix_transform.h"
+
+#include "matrix_transform.h"
